f3ba82d1197ecde460a7773a7d1fa6770f1cf043
Miraj Aryal <mirajaryal@gmail.com>
2022-11-06 16:02:34 +1100

    update assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
